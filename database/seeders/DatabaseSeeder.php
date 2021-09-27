2112792a4f84c0fb52b855b8302d4ef689a8dacf
eulinp <eulinpr@gmail.com>
2021-09-27 07:51:43 -0400

    Dark Theme y Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserTableSeeder;
+use Database\Seeders\InversionTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UserTableSeeder::class);
+        $this->call(InversionTableSeeder::class);
     }
 }
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -34,5 +34,55 @@ class UserTableSeeder extends Seeder
         ];
 
         User::create($user);
+
+
+
+        //Test1
+        User::create([
+            'fullname' => 'Test1',
+            'email' => 'test1@ni.com',
+            'password' => Hash::make('12345678'),
+            'tipo_documento' => 'cedula_ciudadana',
+            'num_documento' => 123456789,
+            'ciudad_residencia' => 'ciudad',
+            'admin' => '0',
+            'direccion_residencia' => 'direccion residencia',
+            'celular' => 04141234567,
+            'banco' => 'Bancolombia',
+            'tipo_cuenta' => 'corriente',
+            'num_cuenta' => '0000-0000-000000000000',
+        ]);
+
+        //Test2
+        User::create([
+            'fullname' => 'Test2',
+            'email' => 'test2@ni.com',
+            'password' => Hash::make('12345678'),
+            'tipo_documento' => 'cedula_extranjera',
+            'num_documento' => 123456789,
+            'ciudad_residencia' => 'ciudad 2',
+            'admin' => '0',
+            'direccion_residencia' => 'direccion residencia 2',
+            'celular' => 04121234567,
+            'banco' => 'Banco A v Villas',
+            'tipo_cuenta' => 'ahorro',
+            'num_cuenta' => '0000-1111-000000000000',
+        ]);
+
+        //Test3
+        User::create([
+            'fullname' => 'Test3',
+            'email' => 'test3@ni.com',
+            'password' => Hash::make('12345678'),
+            'tipo_documento' => 'pasaporte',
+            'num_documento' => 1234567890,
+            'ciudad_residencia' => 'ciudad 3',
+            'admin' => '0',
+            'direccion_residencia' => 'direccion residencia 3',
+            'celular' => 04121234567,
+            'banco' => 'Davivienda',
+            'tipo_cuenta' => 'corriente',
+            'num_cuenta' => '1111-0000-000000000000',
+        ]);
     }
 }
